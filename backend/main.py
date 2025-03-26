c285c443ecc8726e959ba493213e7050c37da873
Ricardo Osburg <r.osburg@uni-muenster.de>
2025-03-26 17:09:34 +0100

    Adresse auf Localhost geänderet

--- backend/main.py
+++ backend/main.py
@@ -12,4 +12,4 @@ async def upload_resume(file: UploadFile = File(...)):
 # Starte den Server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
